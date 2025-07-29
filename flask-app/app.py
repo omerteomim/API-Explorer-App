c4f793119035eba81df64284f183a9e91678858b
omerteomim <omerteomim@gmail.com>
2025-07-29 20:14:26 +0300

    organizing, adding db and docker compose

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,13 +1,44 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, redirect
 import requests
 import time
 import random
+from flask_sqlalchemy import SQLAlchemy
+from models import db, ShoppingItem
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@db:5432/mydb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
+@app.route("/shoppinglist", methods=["GET", "POST"])
+def shoppinglist():
+    if request.method == "POST":
+        item_name = request.form["item_name"].strip()
+        if item_name:
+            new_item = ShoppingItem(name=item_name)
+            db.session.add(new_item)
+            db.session.commit()
+
+    items = ShoppingItem.query.all()
+    return render_template("shoppinglist.html", items=items)
+
+@app.route("/shoppinglist/delete/<int:item_id>", methods=["POST"])
+def delete_item(item_id):
+    item = ShoppingItem.query.get_or_404(item_id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect("/shoppinglist")
+
 
 @app.route("/pokemon", methods=["GET", "POST"])
 def pokemon():
