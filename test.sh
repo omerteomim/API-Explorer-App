44d985de16b0f8dc1f69a30dd251799be90ae318
omerteomim <omerteomim@gmail.com>
2025-07-30 16:38:38 +0300

    add waiting loop

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+start_time=$(date +%s)
+while true; do
+    current_time=$(date +%s)
+    elapsed_time=$(current_time - start_time)
+    if [ $elapsed_time -ge 20 ]; then
+        exit 1
 for route in / /shoppinglist /pokemon /dogs /dadjokes /evilinsult /shoppinglist/delete/; do
     echo "Testing route: $route"
     response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:5000$route)
